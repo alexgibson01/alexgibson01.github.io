fec0f4bdbcc9a315f65a2d813775154154440c04
Alex Gibson <alex.gibson@brighthr.com>
2018-03-22 10:24:39 +0000

    Add files via upload
    New pub and cafe map

---
+++ cafelist.js
@@ -0,0 +1,50 @@
+var cafes = [
+                  
+            {
+              "Type": "Classic Cafe",
+              "Name": "Bar Italia",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.513411,
+              "Longitude": -0.131253,
+              "Postcode": "W1D 4RF"
+            },
+            {
+              "Type": "Modern Cafe",
+              "Name": "NAC",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.512495,
+              "Longitude": -0.153407,
+              "Postcode": "W1K 6ZP"
+            },
+            {
+              "Type": "Cabmen's Shelter",
+              "Name": "Chelsea Embankment",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.483334,
+              "Longitude": -0.167622,
+              "Postcode": "SW3 5HJ"
+            },
+            {
+              "Type": "Cabmen's Shelter",
+              "Name": "Embankment Place",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.506814,
+              "Longitude": -0.122981,
+              "Postcode": "SW3 5HJ"
+            },
+            {
+              "Type": "Cabmen's Shelter",
+              "Name": "Grosvenor Gardens",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.497737,
+              "Longitude": -0.147376,
+              "Postcode": "SW3 5HJ"
+            }
+            
+          ];
+
