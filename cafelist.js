94c8b70eba24aa616720262096255927bf598623
Alex Gibson <alex.gibson@brighthr.com>
2018-03-22 22:11:30 +0000

    Add files via upload
    extra coordinates added

--- cafelist.js
+++ cafelist.js
@@ -34,7 +34,7 @@ var cafes = [
               "County": "London",
               "Latitude": 51.506814,
               "Longitude": -0.122981,
-              "Postcode": "SW3 5HJ"
+              "Postcode": "WC2N 5AQ"
             },
             {
               "Type": "Cabmen's Shelter",
@@ -43,8 +43,44 @@ var cafes = [
               "County": "London",
               "Latitude": 51.497737,
               "Longitude": -0.147376,
-              "Postcode": "SW3 5HJ"
-            }
+              "Postcode": "SW1W 0DH"
+            },
+            {
+              "Type": "Cabmen's Shelter",
+              "Name": "Hanover Square",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.514303,
+              "Longitude": -0.143945,
+              "Postcode": "W1S 1JD"
+            },
+            {
+              "Type": "Cabmen's Shelter",
+              "Name": "Kensington Park Road",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.509976,
+              "Longitude": -0.197960,
+              "Postcode": "W11 3BU"
+             },
+             {
+              "Type": "Cabmen's Shelter",
+              "Name": "Kensington Road",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.501718,
+              "Longitude": -0.183500,
+              "Postcode": "SW7 5DP"
+             },
+             {
+              "Type": "Cabmen's Shelter",
+              "Name": "Pont Street",
+              "Town": "London",
+              "County": "London",
+              "Latitude": 51.497354,
+              "Longitude": -0.158449,
+              "Postcode": "SW1X 9EH"
+             }
             
           ];
 
